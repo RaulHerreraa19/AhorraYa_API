66b496d4d2230f5e290b71d332f213cbc5c24c9d
raulherreraa19 <raulhp320@gmail.com>
2026-01-07 22:46:11 -0600

    added port

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 dns.setDefaultResultOrder('ipv4first')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
@@ -20,6 +20,6 @@ app.use('/auth', authRouter)
 app.use('/users', userRouter)
 app.use('/goals', savingRouter)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
