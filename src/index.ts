1fcf8a0c862a3dc789d79fcc6b239eaff1aaf7c6
raulherreraa19 <raul_herrera@empress.com.mx>
2026-01-12 17:36:12 -0600

    added cors

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import userRouter from './routes/user'
 import savingRouter from './routes/saving'
 import authRouter from './routes/auth'
 import dns from 'dns'
-import cors from 'cors'
+// import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
 dns.setDefaultResultOrder('ipv4first')
 const PORT = 3000
 const app = express()
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 app.use('/auth', authRouter)
