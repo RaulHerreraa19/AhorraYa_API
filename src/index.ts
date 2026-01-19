5585f03f82b17439f0c7a1ad253cbf93c73c4c36
raulherreraa19 <raul_herrera@empress.com.mx>
2026-01-19 13:44:05 -0600

    changes

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import savingRouter from './routes/saving'
 import authRouter from './routes/auth'
 import recordsRouter from './routes/records'
 import dns from 'dns'
-import cors from 'cors'
+//import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
 dns.setDefaultResultOrder('ipv4first')
-const PORT = 3000
+//const PORT = 3000
 const app = express()
-app.use(cors({ origin: '*' }))
+//app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 app.use('/auth', authRouter)
@@ -19,7 +19,3 @@ app.use('/users', userRouter)
 app.use('/savings', savingRouter)
 app.use('/records', recordsRouter)
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
